0034bbe82c3705fd25f4f13b1466effdf7030f38
Stamatis Stroumpis <sstroumpis@intrasoft-intl.com>
2021-05-16 10:19:33 +0300

    * weekDay extension to get day name from Date var

---
+++ WeatherApp/WeatherApp/Extensions/DateExtensions.swift
@@ -0,0 +1,17 @@
+//
+//  DateExtensions.swift
+//  WeatherApp
+//
+//  Created by Stamatis Stroumpis on 16/5/21.
+//
+
+import UIKit
+
+extension Date {
+    func getWeekDay()-> String{
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "EEEE"
+            let weekDay = dateFormatter.string(from: Date())
+            return weekDay
+      }
+}
